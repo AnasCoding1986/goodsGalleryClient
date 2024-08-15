bdaa4c3728ef552629ab5ab2f6e102e91e6819c4
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-15 21:49:11 +0600

    Banner one

--- src/Sections/Banner/Banner.jsx
+++ src/Sections/Banner/Banner.jsx
@@ -1,21 +1,65 @@
-
+import { CiSearch } from "react-icons/ci";
+import b from "../../assets/b.jpg";
+import { FaHashnode } from "react-icons/fa6";
 
 const Banner = () => {
     return (
         <div
             className="hero min-h-screen"
             style={{
-                backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+                backgroundImage: `url(${b})`,
             }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-neutral-content text-center">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                <div className="w-full max-w-screen-lg mx-auto"> {/* Centering the container */}
+                    <h1 className="mb-5 text-5xl font-bold">Welcome To GoodsGallery</h1>
                     <p className="mb-5">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        Choose Your desired Products According To Your Choice
                     </p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                        <div className="border-[1px] border-dotted rounded-md border-white p-4">
+                            <label htmlFor="">Search by product name</label><br />
+                            <div className="flex mt-1">
+                                <input 
+                                    className="p-[4px] flex-grow" 
+                                    type="text" 
+                                    placeholder="Product name" 
+                                    required={true} 
+                                />
+                                <button className="bg-slate-300 p-2 text-red-950 flex-shrink-0">
+                                    <CiSearch />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="border-[1px] border-dotted rounded-md border-white p-4">
+                            <label htmlFor="">Search by Category</label><br />
+                            <div className="flex mt-1">
+                                <input 
+                                    className="p-[4px] flex-grow" 
+                                    type="text" 
+                                    placeholder="Category" 
+                                    required={true} 
+                                />
+                                <button className="bg-slate-300 p-2 text-red-950 flex-shrink-0">
+                                    <CiSearch />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="border-[1px] border-dotted rounded-md border-white p-4">
+                            <label htmlFor="">Sort by price/date</label><br />
+                            <div className="flex mt-1">
+                                <input 
+                                    className="p-[4px] flex-grow" 
+                                    type="text" 
+                                    placeholder="Price/Date" 
+                                    required={true} 
+                                />
+                                <button className="bg-slate-300 p-2 text-red-950 flex-shrink-0">
+                                    <CiSearch />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
